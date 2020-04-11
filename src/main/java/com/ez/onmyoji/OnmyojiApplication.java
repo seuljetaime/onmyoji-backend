8014231ac77ff5a77896bbe62988a1e0b104fa58
seuljetaime <seuljetaime@gmail.com>
2020-04-11 19:23:07 +0800

    spring init

---
+++ src/main/java/com/ez/onmyoji/OnmyojiApplication.java
@@ -0,0 +1,13 @@
+package com.ez.onmyoji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnmyojiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnmyojiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ez/onmyoji/OnmyojiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ez.onmyoji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnmyojiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
